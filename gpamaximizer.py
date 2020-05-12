9ff051c20ea2bf902edd73c4e50f27b27414349e
Ashley LAU Bing Heng <ashley.lau.2018@sis.smu.edu.sg>
2020-05-12 17:41:16 +0800

    Updated output and computational logic
    Computed expected GPA by omitting lowest grade obtained all the way up to the highest grade obtained

--- gpamaximizer.py
+++ gpamaximizer.py
@@ -18,8 +18,14 @@ def check_cu(cu):
     except:
         return False
 
-# cgpa = float(input("what is your current TOTAL GRADE POINTS? (Including this sem)"))
-# num_of_cmod = float(input("How many graded mods have you taken? (Including this sem)"))
+print()
+print("--------------------------------------------------------------------------------")
+print("SMU S/U Decision Maker")
+print("--------------------------------------------------------------------------------")
+print()
+
+cgpa = float(input("what is your current GPA (cGPA)? (Including this sem): "))
+num_of_gmod = float(input("How many graded credit units (CU) have you taken? (Including this sem): "))
 
 # number of mods this sem
 num_of_mod = input("How many graded mods are you taking this sem?: ")
@@ -31,8 +37,10 @@ while not (num_of_mod.isdigit() and int(num_of_mod) > 0):
 p = inflect.engine()
 this_sem = []
 
+print("--------------------------------------------------------------------------------")
+
 for i in range (1, int(num_of_mod) + 1):
-    print("For the " + p.ordinal(i) + " mod,")
+    print("\nFor the " + p.ordinal(i) + " mod,")
     print("How many credit unit (CU) is the module?:")
     cu = input()
 
@@ -49,7 +57,48 @@ for i in range (1, int(num_of_mod) + 1):
         print("What is the grade achieved?")
         grade = str(input())
 
-    this_sem.append([grade.upper(), cu, grades_dict[grade.upper()]])
+    this_sem.append([grade.upper(), float(cu), grades_dict[grade.upper()]])
     this_sem = sorted(sorted(this_sem, key= lambda x: x[1]), key = lambda x: x[-1], reverse=True)
 
-print(this_sem)
+print("--------------------------------------------------------------------------------")
+
+cgpa *= num_of_gmod
+
+max_gpa = {'gpa': 0, 'mods': []}
+
+counter = 0
+for i in this_sem[::-1]:
+    num_of_gmod -= i[1]
+    cgpa -= i[2]
+    current_gpa = round(cgpa/num_of_gmod,2)
+    
+    if (current_gpa > max_gpa['gpa']):
+        max_gpa['gpa'] = current_gpa
+        max_gpa['mods'].append(i)
+
+    if(counter == 0):
+        print("If you S/U 1 " + i[0] + " module with " + str(i[1]) + " credit unit, you will get a cumulative GPA of: " + str(current_gpa))
+    else:
+        print("If you S/U another 1 " + i[0] + " module with " + str(i[1]) + " credit unit, you will get a cumulative GPA of: " + str(current_gpa))
+    
+    counter += 1
+
+print("--------------------------------------------------------------------------------")
+
+print("To get the highest GPA of " + str(max_gpa['gpa']) + " with least number of S/U:")
+print("You should S/U the following modules")
+for i in max_gpa['mods']:
+    print("\tThe " + str(i[1]) + " cu module with grade of " + str(i[0]))
+
+print("--------------------------------------------------------------------------------")
+
+# print("The highest GPA you can get is 3.36 with 3 S/U (2 B modules, 1 B- module)")
+
+#Include this sem CGPA = 3.28
+#Number of mods (include this sem) = 18
+#this sem take 5 mods
+#if SU B- only 3.25
+#if SU B- and B (3 mods) = 3.28
+
+
+# {3.36: {mods:{"B":2, "B-":1}}}
